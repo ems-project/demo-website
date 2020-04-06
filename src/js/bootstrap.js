dc70a17fd4181c4c39bdcefbc8728e33438dd8bc
theus deka <me@theus.be>
2020-04-07 00:36:40 +0200

    First version

---
+++ src/js/bootstrap.js
@@ -0,0 +1,10 @@
+'use strict';
+const $ = require('jquery');
+window.$ = $;
+window.jQuery = $;
+
+import '../css/bootstrap.scss';
+
+require('bootstrap');
+
+console.log('Demo website loaded!');
---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    plugins: [
+        new CopyPlugin([
+            {
+                from: './src/img',
+                to: 'img'
+            },
+        ], {
+            ignore: [{
+                dots: true,
+                glob: 'samples/**/*'
+            },{
+                dots: true,
+                glob: 'adapters/**/*'
+            },{
+                dots: true,
+                glob: '.github/**/*'
+            },{
+                dots: true,
+                glob: '**/*.php'
+            }]
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "css/[name].css",
+            chunkFilename: "[id].css"
+        }),
+    ],
+    context: path.resolve(__dirname, './'),
+    entry: {
+        'index': './src/js/bootstrap.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js',
+        //publicPath: '../bundles/emscore/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        // you can specify a publicPath here
+                        // by default it use publicPath in webpackOptions.output
+                        publicPath: '../'
+                    }
+                },{
+                    loader: 'css-loader', // translates CSS into CommonJS
+                    options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: 'less-loader' // compiles Less to CSS
+                }]
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        // you can specify a publicPath here
+                        // by default it use publicPath in webpackOptions.output
+                        publicPath: '../'
+                    }
+                },{
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                    // 'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: 'media/[name].[ext]',
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    }
+};
