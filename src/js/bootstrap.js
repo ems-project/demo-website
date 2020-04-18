b0975a957c472a39dddc6be6760d68d740597db2
theus deka <me@theus.be>
2020-04-18 08:53:36 +0200

    Back to top JS and CSS

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -7,5 +7,8 @@ import '../css/bootstrap.scss';
 import adminMenu from '@elasticms/admin-menu';
 require('bootstrap');
 
+import back2top from "./back2top";
+
+back2top();
 adminMenu();
 console.log('Demo website loaded!');
